ea44777b29a9aacd8758f9eba958de9a4c43af3c
Gerardo Navarro Suarez <gerardo.navarro-suarez@student.hpi.uni-potsdam.de>
2013-07-09 15:30:01 +0200

    Added some testcase and improved implementation

--- multiples_of_3_and_5/solution_algorithm.rb
+++ multiples_of_3_and_5/solution_algorithm.rb
@@ -1,6 +1,6 @@
 class SolutionAlgorithm
 
-  # 1st implementation
+  # 1st implementation - O(number)
   # def SolutionAlgorithm.muliples_of_3_and_5_for(number)
     
   #   sum = 0
@@ -10,20 +10,43 @@ class SolutionAlgorithm
   #     end
   #   end
 
+  #   puts ""
+  #   puts sum
+  
   #   sum
   # end
 
-  # 2nd implementation
+  # 2nd implementation - O(number)
+  # def SolutionAlgorithm.muliples_of_3_and_5_for(number)
+    
+  #   amount_of_numbers_divided_by_3 = (number - 1) / 3 
+  #   amount_of_numbers_divided_by_5 = (number - 1) / 5
+
+  #   sum = 0
+  #   1.upto(amount_of_numbers_divided_by_3) do |i|
+  #     sum += 3 * i unless i % 5 == 0
+  #     sum += 5 * i if i <= amount_of_numbers_divided_by_5
+  #   end
+
+  #   puts ""
+  #   puts sum
+
+  #   sum
+  # end
+
+  # 3rd implementation - O(1)
   def SolutionAlgorithm.muliples_of_3_and_5_for(number)
     
     amount_of_numbers_divided_by_3 = (number - 1) / 3 
+    amount_of_multiples_of_15_in_number_divided_by_3 = amount_of_numbers_divided_by_3 / 5
     amount_of_numbers_divided_by_5 = (number - 1) / 5
 
-    sum = 0
-    1.upto(amount_of_numbers_divided_by_3) do |i|
-      sum += 3 * i unless i % 5 == 0
-      sum += 5 * i if i <= amount_of_numbers_divided_by_5
-    end
+    sum = 3 * (amount_of_numbers_divided_by_3 * (amount_of_numbers_divided_by_3 + 1) / 2)
+    sum += 5 * (amount_of_numbers_divided_by_5 * (amount_of_numbers_divided_by_5 + 1) / 2)
+    sum -= 15 * (amount_of_multiples_of_15_in_number_divided_by_3 * (amount_of_multiples_of_15_in_number_divided_by_3 + 1) / 2)
+    
+    puts ""
+    puts sum
 
     sum
   end
--- multiples_of_3_and_5/testcase.rb
+++ multiples_of_3_and_5/testcase.rb
@@ -14,6 +14,7 @@ class ProblemTestCase < Test::Unit::TestCase
 
   def test_hard
     assert_equal(233168, SolutionAlgorithm.muliples_of_3_and_5_for(1000))
+    assert(233168 < SolutionAlgorithm.muliples_of_3_and_5_for(1000000000))
   end
  
 end
