d1325ebb011674a6b77ad38eda4b77ef24decf03
Gerardo Navarro Suarez <gerardo.navarro-suarez@student.hpi.uni-potsdam.de>
2013-07-09 14:53:55 +0200

    Added some testcase and improved implementation

--- multiples_of_3_and_5/solution_algorithm.rb
+++ multiples_of_3_and_5/solution_algorithm.rb
@@ -1,13 +1,30 @@
 class SolutionAlgorithm
 
+  # 1st implementation
+  # def SolutionAlgorithm.muliples_of_3_and_5_for(number)
+    
+  #   sum = 0
+  #   number.times do |i|
+  #     if (i % 3 == 0) or (i % 5 == 0)
+  #       sum += i
+  #     end
+  #   end
+
+  #   sum
+  # end
+
+  # 2nd implementation
   def SolutionAlgorithm.muliples_of_3_and_5_for(number)
     
+    amount_of_numbers_divided_by_3 = (number - 1) / 3 
+    amount_of_numbers_divided_by_5 = (number - 1) / 5
+
     sum = 0
-    number.times do |i|
-      if (i % 3 == 0) or (i % 5 == 0)
-        sum += i
-      end
+    1.upto(amount_of_numbers_divided_by_3) do |i|
+      sum += 3 * i unless i % 5 == 0
+      sum += 5 * i if i <= amount_of_numbers_divided_by_5
     end
+
     sum
   end
  
--- multiples_of_3_and_5/testcase.rb
+++ multiples_of_3_and_5/testcase.rb
@@ -8,6 +8,10 @@ class ProblemTestCase < Test::Unit::TestCase
     assert_equal(23, SolutionAlgorithm.muliples_of_3_and_5_for(10))
   end
 
+  def test_overlapping_case
+    assert_equal(78, SolutionAlgorithm.muliples_of_3_and_5_for(20))
+  end
+
   def test_hard
     assert_equal(233168, SolutionAlgorithm.muliples_of_3_and_5_for(1000))
   end
